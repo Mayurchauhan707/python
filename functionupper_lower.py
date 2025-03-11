ef82252bf01d6e960658a609a1589171c3fae862
Mayurchauhan707 <mayurchauhan3260@gmail.com>
2025-03-11 18:01:45 +0530

    Add files via upload

--- functionupper_lower.py
+++ functionupper_lower.py
@@ -1,4 +1,5 @@
 def count_lower_upper(str):
+    
     l=0
     u=0
     for i in str :
@@ -6,15 +7,13 @@ def count_lower_upper(str):
         l=l+1
       elif i>='A'and i<='Z':
         u=u+1
+    print("lower count :",l,"upper count:",u)
 """  for i in str :
       if(i.isupper()):
         u=u+1
       elif(i.islower()):
 l=l+1"""
-    print("lower count :",l,"upper count:",u)
-
-
-
+    
 
 str=input("enter the string : ")
 count_lower_upper(str)
